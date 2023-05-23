b846e7f736500a2d38f343c3d469cc992fc04a49
ruan.fachini <ruanvfachini@gmail.com>
2023-05-22 21:41:04 -0300

    1-Criado gráfico temporal - Coronavirus(2020).2-Ajuste no gráfico hierarquico

--- visualizacao_hierarquica.py
+++ visualizacao_hierarquica.py
@@ -1,7 +1,6 @@
 #https://data.world/toscanocombr/mapainvestimentomgbrasil2019 - link para dataset
-
 import plotly.express as px
-from pandas import read_csv, DataFrame, Series, arrays, SparseDtype
+from pandas import read_csv, DataFrame
 
 df = read_csv(".\DataSets\mapa_investimento_mg_brasil_2019.csv", header=0, delimiter=';')
 
@@ -12,29 +11,7 @@ orgaos = df["Orgao"].values
 items_de_despesa = df["Item de Despesa"].values
 valor_investido = df["Valor Investido"].apply(lambda x: (x.replace(".", "").replace(",", "."))).astype(float).values
 
-# , "Item", "Y2019"
-
 df = DataFrame(dict(municipios=municipios, orgaos=orgaos, items_de_despesa=items_de_despesa))
 df["all"] = "all"
 fig = px.treemap(df,path=[municipios,orgaos,items_de_despesa],values=valor_investido, title="Mapa de Investimento da cidade de Belo Horizonte, Minas Gerais,Brasil (2019)")
 fig.show()
-
-
-
-# print(data)
-
-# df = DataFrame(data)
-# df["all"] = "all" #garante um único nó raiz
-# fig = px.treemap(df,path=[regioes,estados],values=populacao)
-# fig.show()
-
-
-
-
-# # fig = px.sunburst(
-# #     data,
-# #     names='character',
-# #     parents='parent',
-# #     values='value',
-# # )
-# # fig.show()
--- visualizacao_temporal.py
+++ visualizacao_temporal.py
@@ -1,19 +1,40 @@
-from pandas import read_csv
+# https://data.world/kgarrett/covid-19-open-research-dataset - link para dataset
+from pandas import read_csv, Timestamp
 import numpy as np
 from matplotlib import pyplot as plt
 
-df = read_csv(".\DataSets\covid_12-04-2020.csv")
-subjects = ["new_cases"]
-dataset = df.groupby(["date", "location"])[subjects].mean()
+df = read_csv(  ".\DataSets\covid_12-04-2020.csv",
+                parse_dates=['date'],
+                date_format="%Y-%m-%d")
 
-contries_count = df['location'].unique()
+df = df[df['location'] == 'Brazil']
+df = df[df['date'].dt.strftime('%Y') == '2020']
 
-for i in contries_count:
-    
+df = df.filter(['date', 'total_cases','total_deaths', 'weekly_deaths'])
+df = df.resample(rule='M', on='date').sum()
 
-# indx = np.arange(len(subjects))
-# score_labels = np.array(0,110,10)
+x = np.arange(len(df))
 
+total_cases = df['total_cases'].values
+total_deaths = df['total_deaths'].values
+weekly_deaths = df['weekly_deaths'].values
+width = 0.2
+  
+plt.bar(x-width, total_cases, width, color='cyan')
+plt.bar(x, total_deaths, width, color='orange')
+plt.bar(x+width, weekly_deaths, width, color='green')
 
-# print(dataset)
-# plt.show()
+monthStickList = []
+
+for date in df.axes[0].values:
+    monthStickList.append(Timestamp(date).strftime("%m/%Y"))
+
+plt.xticks(x, monthStickList)
+plt.xlabel("Meses")
+plt.ylabel("Médias")
+plt.legend(["Total de casos", "Total de mortes", "Mortes semanais"])
+plt.ticklabel_format(style='sci', axis='y', useMathText=True)
+
+plt.yticks(np.arange(plt.ylim()[0], plt.ylim()[1], 10000000))
+
+plt.show()
